b8ce2ca17e24b1f1034dc34596c8d2be2a8737b1
Foti Kerkeshi <foti.kerkeshi@hotmail.com>
2020-07-23 13:45:47 +0200

    update

--- 03_euler.py
+++ 03_euler.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python3
 
-def SieveOfEratosthenes(n): 
-  pp = []
-  # Create a boolean array "prime[0..n]" and initialize 
-  # all entries it as true. A value in prime[i] will 
-  # finally be false if i is Not a prime, else true. 
-  prime = [True for i in range(n + 1)] 
-  p = 2
-  while (p * p <= n): 
-        
-      # If prime[p] is not changed, then it is a prime 
-      if (prime[p] == True): 
-            
-          # Update all multiples of p 
-          for i in range(p * 2, n + 1, p): 
-              prime[i] = False
-      p += 1
-  prime[0]= False
-  prime[1]= False
-  # Print all prime numbers 
-  for p in range(n + 1): 
-      if prime[p]: 
-          pp.append(p)
-          print(p)
+# The idea is basically the same as writing
+# the number as product of prime numbers
 
-SieveOfEratosthenes(300425737572)
+n = 600851475143
+
+for i in range(2, 10000):
+  while n%i == 0:
+    n //= i
+    if n == 1 or n == i:
+      print(i)
+      break
